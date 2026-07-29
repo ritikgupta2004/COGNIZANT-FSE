f83eda1ce1648dd5b463d9760a4d9be9b0befeaa
Ritik Gupta <ritikgupta8170@gmail.com>
2026-07-30 01:10:38 +0530

    Added Exercise 36 - HTTP Client API

---
+++ Core-Java/HttpClientExample.java
@@ -0,0 +1,27 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class HttpClientExample {
+    public static void main(String[] args) {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://api.github.com"))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response =
+                    client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println("Status Code: " + response.statusCode());
+            System.out.println("\nResponse Body:");
+            System.out.println(response.body());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
